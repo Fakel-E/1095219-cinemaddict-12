e71b683fc1114751b8f8e6dd1e79ca612d8ecda0
Evgeniy Semenov <SemenovEvg1990@gmail.com>
2020-08-09 11:31:04 +0300

    Поправил ESLint

--- src/main.js
+++ src/main.js
@@ -188,7 +188,7 @@ const createTopRatedTemplate = () => {
 
 const createTopCommentTemplate = () => {
   return (
-      `<section class="films-list--extra">
+    `<section class="films-list--extra">
       <h2 class="films-list__title">Most commented</h2>
 
       <div class="films-list__container">
